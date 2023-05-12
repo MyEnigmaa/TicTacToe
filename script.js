8386527d5f6820de3189bb8ff6c22ce7aba1c1d8
Nikolas <n.koeck@gmx.de>
2023-05-12 20:35:29 +0200

    center icons, change symbols onclick

--- script.js
+++ script.js
@@ -3,27 +3,42 @@ const board = document.getElementById("board");
 const newGameBtn = document.getElementById("newGame");
 const xSource = "./img/X_icon.svg";
 const oSource = "./img/O_icon.svg";
-var plates;
+var player = true;
 
 const gameboard = (() => {
     const boardArr = new Array(3).fill(0).map(()=>new Array (3).fill(0));
 
     function createTile(number, row, column){
+        number = number;
+        tileRow = row;
+        tileColumn = column;
+        tileTaken = false;
+        var divTile;
         return{
-            number: number,
-            tileRow: row,
-            tileColumn: column,
-            tileTaken: false,
-            selectTile(){
+
+            selectTile(e){
+                if(this.tileTaken==true || e.target != e.currentTarget) return;
+                let y = document.createElement("img");
+                if(player==true){
+                    y.src = xSource;
+                    player = false;
+                }
+                else {
+                    y.src = oSource;
+                    player = true;
+                }
+                y.className = "pressed-img";
+                e.target.appendChild(y);
                 tileTaken=true;
             },
             createDomElement(){
-                let temp = document.createElement("div");
-                temp.id = number;
-                temp.className = "plate";
-                board.appendChild(temp);
-                temp.addEventListener("click", (element)=>{platePressed(element);})
-            }
+                divTile = document.createElement("div");
+                divTile.id = number;
+                divTile.className = "plate";
+                board.appendChild(divTile);
+                divTile.addEventListener("click", (element)=>{platePressed(element);})
+            },
+
         }
     }
 
@@ -43,26 +58,35 @@ const gameboard = (() => {
         createBoard();
     };
 
-    const platePressed = (e) => {
-        if(e.target != e.currentTarget)return; //prevents childs from triggering
 
-        if(!e.target.firstChild ){
-            let y = document.createElement("img");
-            y.src = xSource;
-            y.style.display = "block";
-            e.target.appendChild(y);
+
+
+    return {createBoard, clearBoard, boardArr};
+})();
+
+
+const game = (()=>{
+    var timesPressed = 0;
+
+    const checkForWin = ()=>{
+
+        
+
+        if(timesPressed==8){
+            prompt("no Winner")
         }
     }
 
 
-    return {createBoard, clearBoard};
 })();
-
 
 //Functions
 newGameBtn.addEventListener("click", ()=>{gameboard.clearBoard()}); //Clears the Board
 
-
+function platePressed(e){
+    if(e.target != e.currentTarget)return; //prevents childs from triggering
+    gameboard.boardArr[Math.floor(e.target.id/3)][e.target.id%3].selectTile(e);
+}
 
 function player(){
 }
