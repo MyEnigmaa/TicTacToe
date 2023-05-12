5ed9e25d7c9dd110d38ae82807ed5c6ab8f3d2b8
Nikolas <n.koeck@gmx.de>
2023-05-12 14:57:48 +0200

    add newGameBtn, prevent from taking same tile twice

--- script.js
+++ script.js
@@ -1,26 +1,58 @@
 //Variables & Objectes
 const board = document.getElementById("board");
+const newGameBtn = document.getElementById("newGame");
+const xSource = "./img/X_icon.svg";
+const oSource = "./img/O_icon.svg";
+var plates;
+
 const gameboard = (() => {
     const boardArr = new Array(3).fill(0).map(()=>new Array (3).fill(0));
 
-    const createBoard = () =>{
-        
+
+    const createBoard = () => {
+        let x=0;
         for(i=0;i<3;i++){
             for(j=0;j<3;j++){
-                boardArr[i][j] = document.createElement("div");
+                let temp = document.createElement("div");
+                boardArr[i][j] = temp;
+                boardArr[i][j].id = x;
                 boardArr[i][j].className = "plate";
                 board.appendChild(boardArr[i][j]);
+                x++;
             }
-
         }
+        plates = document.querySelectorAll(".plate");
+        plates.forEach(element => {
+            element.addEventListener("click", (element)=>{
+                platePressed(element);
+            })
+        });
+    };
+    const clearBoard = () => {
+        board.innerHTML = '';
+        createBoard();
     };
 
-    return {createBoard};
+    const platePressed = (e) => {
+        if(e.target != e.currentTarget)return; //prevents childs from triggering
+
+        if(!e.target.firstChild ){
+            let y = document.createElement("img");
+            y.src = xSource;
+            y.style.display = "block";
+            e.target.appendChild(y);
+        }
+    }
+    return {createBoard, clearBoard};
 })();
 
 
-function player(){
+//Functions
+newGameBtn.addEventListener("click", ()=>{gameboard.clearBoard()}); //Clears the Board
 
+
+
+function player(){
 }
 
 window.onload = ()=>{
