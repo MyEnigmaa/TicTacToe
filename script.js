095b49b7147703b1f7e61d3f16d21ce618e4e84b
Nikolas <n.koeck@gmx.de>
2023-05-12 15:15:53 +0200

    add tile factoryFunction

--- script.js
+++ script.js
@@ -8,25 +8,35 @@ var plates;
 const gameboard = (() => {
     const boardArr = new Array(3).fill(0).map(()=>new Array (3).fill(0));
 
+    function createTile(number, row, column){
+        return{
+            number: number,
+            tileRow: row,
+            tileColumn: column,
+            tileTaken: false,
+            selectTile(){
+                tileTaken=true;
+            },
+            createDomElement(){
+                let temp = document.createElement("div");
+                temp.id = number;
+                temp.className = "plate";
+                board.appendChild(temp);
+                temp.addEventListener("click", (element)=>{platePressed(element);})
+            }
+        }
+    }
+
 
     const createBoard = () => {
         let x=0;
         for(i=0;i<3;i++){
             for(j=0;j<3;j++){
-                let temp = document.createElement("div");
-                boardArr[i][j] = temp;
-                boardArr[i][j].id = x;
-                boardArr[i][j].className = "plate";
-                board.appendChild(boardArr[i][j]);
+                boardArr[i][j] = createTile(x,i,j);
+                boardArr[i][j].createDomElement();
                 x++;
             }
         }
-        plates = document.querySelectorAll(".plate");
-        plates.forEach(element => {
-            element.addEventListener("click", (element)=>{
-                platePressed(element);
-            })
-        });
     };
     const clearBoard = () => {
         board.innerHTML = '';
@@ -43,6 +53,8 @@ const gameboard = (() => {
             e.target.appendChild(y);
         }
     }
+
+
     return {createBoard, clearBoard};
 })();
 
